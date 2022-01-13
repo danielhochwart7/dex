6c9a184bbb3912a7cdb879757eb77175378ba901
Daniel Hochwart <daniel.hochwart7@gmail.com>
2022-01-13 18:11:56 +0100

    Dex initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1 class="text-5xl font-bold underline">
+        Dex TBD
+      </h1>      
+    </div>
+  )
+}
+
+export default App
