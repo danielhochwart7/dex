beb647ab6cbf259317be6c01fdc19df6e0abf18b
Daniel Hochwart <daniel.hochwart7@gmail.com>
2022-01-16 15:28:29 +0100

    Provide more unit tests for contracts

--- test/Dex.test.js
+++ test/Dex.test.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai")
+
 const Dex = artifacts.require("Dex")
 const Dho = artifacts.require("Dho")
 
@@ -8,29 +10,52 @@ require("chai")
 const dexContractName = "My Dex"
 const dhoTokenSymbol = "DHO"
 
-contract("Dex", (accounts) => {
+function tokens(n) {
+    return web3.utils.toWei(n, "ether")
+}
+
+contract("Dex", ([deployer, investor]) => {
     let dho, dex
 
     before(async () => {
         dho = await Dho.new()
         dex = await Dex.new(dho.address)
+
+        dho.transfer(dex.address, tokens("1000000"))
     })
 
     describe("Dex deployment", async () => {
-        it("Should have a name", async() => {
+        it("Should have a name", async () => {
             const name = await dex.name()
             assert.equal(name, dexContractName)
         })
+        it("Should have Dho tokens", async() => {
+            const dexTokenBalance = await dho.balanceOf(dex.address)
+            assert.equal(dexTokenBalance, tokens("1000000"))
+        })
     })
 
-    describe("DHO deployment", async () => {
+    describe("Dho Token deployment", async () => {
         it("Should have symbol DHO", async() => {
             const symbol = await dho.symbol()
             assert.equal(symbol, dhoTokenSymbol)
         })
-        it("Should have name Token Dho", async() => {
+        it("Should have name Dho Token", async() => {
             const name = await dho.name()
             assert.equal(name, "Dho Token")
         })
     })
+
+    describe("buyTokens()", async = () => {
+        it("Sould transfer tokens to investor based on a token fixed price of 1ETH:100DHO", async () => {
+            const result = await dex.buyTokens({from: investor, value: tokens("1")}) // 1 ETH
+            const investorTokenBalance = await dho.balanceOf(investor)
+            assert.equal(investorTokenBalance, tokens("100")) // 100 DHO
+            console.log(result)
+        })
+    })
+
+
+
+
 })
