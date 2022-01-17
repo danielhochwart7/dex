f5a25a315319cd27d74282ce1b0ea97451a7dee4
Daniel Hochwart <daniel.hochwart7@gmail.com>
2022-01-17 16:51:45 +0100

    Reposition items on screen

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,28 +1,37 @@
 const Body = () =>  {
     return (
         <div className="flex flex-col justify-center items-center p-5">
-            <div className="flex flex-col w-full md:w-80 lg:w-80 items-center py-3 px-3 white-glassmorphism space-y-10">
+            <div className="flex flex-col w-full md:w-80 lg:w-80 py-5 px-3 white-glassmorphism space-y-5">
                 <div className="flex flex-row relative">
-                    <div className="text-center font-size absolute right-10 white-glassmorphism px-6 hover:bg-slate-600">
+                    <button type="button" className="rounded-lg text-center font-bold font-size absolute right-10 white-glassmorphism px-6 hover:bg-slate-600">
                         Buy
-                    </div>
-                    <div className="text-center font-size absolute left-10 white-glassmorphism px-6 hover:bg-slate-600">
+                    </button>
+                    <button type="button" className="rounded-lg text-center font-bold font-size absolute left-10 white-glassmorphism px-6 hover:bg-slate-600">
                         Sell
-                    </div>
+                    </button>
                 </div>
-                <div className="text-center font-size">
-                    Eth balance: 3.8736
+                <div className="font-bold">
+                    ETH balance: 3.8736...
                 </div>
                 <div className="flex w-full items-center py-5 white-glassmorphism justify justify-evenly relative">
-                    <div className="text-center absolute right-1 h-7 w-14 white-glassmorphism hover:bg-slate-600">
+                    <div className="flex absolute left-3">
+                        3.8736
+                    </div>
+                    <div className="rounded-l-lg text-center font-bold absolute right-1 h-7 w-14 white-glassmorphism hover:bg-slate-600">
                         ETH
                     </div>
                 </div>
                 <div className="flex w-full items-center py-5 white-glassmorphism justify justify-evenly relative">
-                    <div className="text-center absolute right-1 h-7 w-14 white-glassmorphism hover:bg-slate-600">
+                    <div className="flex absolute left-3">
+                        387.36
+                    </div>
+                    <div className="rounded-l-lg text-center font-bold absolute right-1 h-7 w-14 white-glassmorphism hover:bg-slate-600">
                         DHO
                     </div>
                 </div>
+                <button type="button" onClick={() => {}} className="flex justify-center bg-[#2952e3] px-6 py-1 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
+                    <p className="text-white text-base font-semibold">Swap</p>
+                </button>
             </div>
             
         </div>
