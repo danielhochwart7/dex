7dc67282db377f8b27168ad703f455b6cd2c4b78
Daniel Hochwart <daniel.hochwart7@gmail.com>
2022-01-18 16:18:43 +0100

    Provided wallet connection function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,25 @@
+import React, { useContext } from "react";
+import { ConnectionContext } from "../context/ConnectionContext";
 import logo from "../images/logo.png";
 
 const Header = () =>  {
+    const { connectWallet, connectedAccount } = useContext(ConnectionContext);
+
     return (
         <div className="w-full flex flex-row items-center py-5">
             <div className="flex p-4 px-12">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
             
-            {true ? (
-                <button type="button" onClick={() => {}} className="flex absolute right-5  bg-[#2952e3] p-2 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
+            {!connectedAccount ? (
+                <button type="button" onClick={connectWallet} className="flex absolute right-5  bg-[#2952e3] p-2 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
                     <p className="text-white text-base font-semibold">Connect Wallet</p>
                 </button>
             ) : (
-                <button type="button" onClick={() => {}} className="flex absolute right-5  bg-[#2952e3] p-2 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
-                    <p className="text-white text-base font-semibold">0x327d89...923</p>
+                <button type="button" className="flex absolute right-5  bg-[#2952e3] p-2 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
+                    <p className="text-white text-base font-semibold">{ connectedAccount }</p>
                 </button>
             )}
-            
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { ConnectionProvider } from './context/ConnectionContext'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ConnectionProvider>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+  </ConnectionProvider>,
   document.getElementById('root')
 )
