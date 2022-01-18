8020d8c10db520bba86df6e02a24a52714cd06b9
Daniel Hochwart <daniel.hochwart7@gmail.com>
2022-01-18 15:13:30 +0100

    Show address when connected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,17 @@ const Header = () =>  {
             <div className="flex p-4 px-12">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
-            <button type="button" onClick={() => {}} className="flex absolute right-5  bg-[#2952e3] p-2 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
-                <p className="text-white text-base font-semibold">Connect Wallet</p>
-            </button>
+            
+            {true ? (
+                <button type="button" onClick={() => {}} className="flex absolute right-5  bg-[#2952e3] p-2 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
+                    <p className="text-white text-base font-semibold">Connect Wallet</p>
+                </button>
+            ) : (
+                <button type="button" onClick={() => {}} className="flex absolute right-5  bg-[#2952e3] p-2 rounded-full cursor-pointer hover:bg-[#2546bd] shadow-lg">
+                    <p className="text-white text-base font-semibold">0x327d89...923</p>
+                </button>
+            )}
+            
         </div>
     )
 }
