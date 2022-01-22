7ca0fa89e105c40fdbd2efbc2bb38e5cc5e53bde
Daniel Hochwart <daniel.hochwart7@gmail.com>
2022-01-22 12:14:18 +0100

    Added a BuyForm

--- src/components/SwapMenu.jsx
+++ src/components/SwapMenu.jsx
@@ -1,14 +1,9 @@
-import { useContext, useState } from "react";
-import { ConnectionContext } from "../context/ConnectionContext";
+import BuyForm from "./BuyForm";
 
 const SwapMenu = () =>  {
-    const { accountEthBalance, accountDhoBalance } = useContext(ConnectionContext);
-    const [ output, setOutput ] = useState(0);
-    const tokenRate = 100;
 
     return (
-        <div className="flex flex-col justify-center items-center p-5">
-            <div className="flex flex-col w-full md:w-80 lg:w-80 py-5 px-3 white-glassmorphism space-y-5">
+        <div className="flex flex-col justify-center items-center space-y-9">
                 <div className="flex flex-row relative">
                     <button type="button" onClick={() => setIsBuy(true)} className="rounded text-center font-bold font-size absolute right-10 white-glassmorphism px-6 hover:bg-slate-600">
                         Buy
@@ -17,47 +12,7 @@ const SwapMenu = () =>  {
                         Sell
                     </button>
                 </div>
-                <div className="font-bold">
-                    ETH balance: {accountEthBalance}
-                </div>
-                <div className="flex rounded w-full items-center py-5 white-glassmorphism justify justify-evenly relative">
-                    <div className="flex absolute left-3">
-                        <input
-                            type="number"
-                            onChange={(event) => {
-                                setOutput(event.target.value * tokenRate); // 1 ETH = 100 DHO
-                            }}
-                            className=" text-white font-semibold white-glassmorphism px-1 rounded-lg"
-                            placeholder="0"
-                            required
-                        />
-                    </div>
-                    <div className="rounded text-center font-bold absolute right-1 h-7 w-14 white-glassmorphism hover:bg-slate-600">
-                        ETH
-                    </div>
-                </div>
-                <div className="font-bold">
-                    DHO balance: {accountDhoBalance}
-                </div>
-                <div className="flex rounded w-full items-center py-5 white-glassmorphism justify justify-evenly relative">
-                    <div className="flex absolute left-3">
-                        <input
-                            type="number"
-                            className=" text-white font-semibold white-glassmorphism px-1 rounded-lg"
-                            placeholder="0"
-                            value={output}
-                            disabled
-                        />
-                    </div>
-                    <div className="rounded text-center font-bold absolute right-1 h-7 w-14 white-glassmorphism hover:bg-slate-600">
-                        DHO
-                    </div>
-                </div>
-                <button type="button" onClick={() => {}} className="flex rounded justify-center bg-[#2952e3] px-6 py-1 cursor-pointer hover:bg-[#2546bd] shadow-lg">
-                    <p className="text-white text-base font-semibold">Swap</p>
-                </button>
-            </div>
-            
+                <BuyForm />
         </div>
     )
 }
